4d394660186616567e21e67c3345f1d90963af53
MohammadIsCoding1 <80381539+MohammadIsCoding1@users.noreply.github.com>
2021-06-03 16:41:39 -0500

    Update inject.py

--- inject.py
+++ inject.py
@@ -1,3 +1,20 @@
 import request
 
-payload=''
+
+def check_vuln():
+  target=raw_input("Url inject: ")
+  payload="<script>alert(XSS);<script>"
+
+  req=request.post(target + payload) dd
+
+  if payload in req.text:
+    print("XSS vulnerability discovered")
+    print("Attacking payload : "+payload)
+  else:
+    print("Secure")
+def code_inject():
+  pass
+    
+    
+if __name__=="__main__":
+  check_vuln()
