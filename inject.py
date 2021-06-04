4165e78881a86ef804301cd7219117286436708b
MohammadIsCoding1 <80381539+MohammadIsCoding1@users.noreply.github.com>
2021-06-04 08:31:06 -0500

    Update inject.py

--- inject.py
+++ inject.py
@@ -16,11 +16,51 @@ def main():
     > ''')
     if choice=1:
       inject_payload=open('injection.js','r').read()
+  
+  if type=="2":
+    choice=input('''
+    [1]template_injection.html
+    [2]basic_injection.html
+    [3]xss.html
+    
+    > ''')
+    
+    if choice=='1':
+      inject_payload=open('template_injection.html','r').read()
       
+      while True:
+        print('payload injection is sucessful')
+        break
       
-  payload=inject
+      while False:
+        print('payload injection fail')
+        break
+    
+    if choice=='2':
+      inject_payload=open('basic_injection.html','r').read()
+      
+      while True:
+        print('payload injection is sucessful')
+        break
+      
+      while False:
+        print('payload injection fail')
+        break
+        
+    if choice=='3':
+      inject_payload=open('xss.html','r').read()
+      
+      while True:
+        print('payload injection is sucessful')
+        break
+      
+      while False:
+        print('payload injection fail')
+        break
+    
+  payload=inject_payload
 
-  req=request.post(target + payload) dd
+  req=request.post(target + payload)
 
   if payload in req.text:
     print("=vulnerability discovered")
